a2fc61d5bea2b2bf8f18dd92787f192e9d06fa1a
Kasparas Bareika <kasparas12@gmail.com>
2019-04-16 16:52:15 +0300

    pakeitimas8

--- public_html/index.php
+++ public_html/index.php
@@ -1,12 +1,12 @@
 <!DOCTYPE html>
-<<html>
+<html>
     <head>
-        <title>Aš valdau HTML</title>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+        <title><?php print 'PHP lydės' . strtotime ('+20 hours'); ?></title>
     </head>
     <body>
-        <h1> Vardas - HTML asas</h1>
-        <p>moku kurti statinį turinį</p>
-
+        <h1>Kasparas - PHP su manim buvo ir <?php print strtotime('-1 hour') . 'valandą'; ?></h1>
+        <p>Sekantys metai <?php print strtotime('next year'); ?></p>
     </body>
 </html>
 
