02199a978f2777383163e89304c4ab48e9f6f310
Kasparas Bareika <kasparas12@gmail.com>
2019-04-16 23:24:16 +0300

    random numbers,

--- public_html/index.php
+++ public_html/index.php
@@ -6,14 +6,14 @@ date_default_timezone_set('Europe/Vilnius');
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
-        <title><?php // Komentarai ?> </title> 
-    </head> 
-    <body> 
-        <h1><?php // Dainų tekstai ?></h1>
-        <h2><?php // Dainų tekstai ?></h2>
-        <p><?php // Aš toks vienišas jaunas paragrafas... ?></p>
-<!--        <p><?php // Mindau svetimo kodo gatves ?></p>-->
-    </body>
+        <title>PHP lydės ir<?php print" ".mt_rand(2019, 2029)."-".mt_rand(1, 12)."-".mt_rand(1, 31); ?> </title> 
+    </head>
+    <body>
+        <h1>Kasparas-Galbūt turėsiu <?php print  rand (1, 5); ?> vaikų/us!</h1>
+            <img src="https://media1.tenor.com/images/9cd39f6103e2a6cad13a8a54b4e1ad99/tenor.gif?itemid=8777247" alt="microsoftlogo" style="height:400px;">
+            <p style="color:teal;">D.Trumpas nebebus prezidentu <?php print date(rand(2021, 2031)) . date(' M-d'); ?>.</p>
+            <img src="https://media0.giphy.com/media/xTiTnHXbRoaZ1B1Mo8/giphy.gif" alt="trumpas">
+     </body>
 </html>
 
 
