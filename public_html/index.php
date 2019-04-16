4880e93c9781cdf9073fabf41b1b7b6c6162f9c3
Kasparas Bareika <kasparas12@gmail.com>
2019-04-16 14:55:20 +0300

    pakeitimas2

--- public_html/index.php
+++ public_html/index.php
@@ -1,2 +1,14 @@
-<?php print 'Laba diena'; ?>
+<!DOCTYPE html>
+<<html>
+    <head>
+        <title>Aš valdau HTML</title>
+    </head>
+    <body>
+        <h1> Vardas - HTML asas</h1>
+        <p>moku kurti statinį turinį</p>
+
+    </body>
+</html>
+
+
 
