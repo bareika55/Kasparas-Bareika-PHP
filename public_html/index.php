0e663fdb7e3ddade4ce2e46dd2c57f9c5138c70d
Kasparas Bareika <kasparas12@gmail.com>
2019-04-17 21:29:57 +0300

    Bomba !

--- public_html/index.php
+++ public_html/index.php
@@ -1,27 +1,19 @@
 <?php
 
-$siukslines_turis = 50; //(dm3)
-$siuksliu_turis_avg_per_day = 12;
-$max_kaupo_turis = rand(12, 50);
+$page = $_SERVER['PHP_SELF'];
+$sec = "1";
 
 ?>
-
 <!DOCTYPE html>
 <html>
     <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
-        <title>PHP lydės ir</title> 
+        <meta charset="UTF-8">
+    <meta http-equiv="refresh" content="<?php echo $sec?>;URL='<?php echo $page?>'">
     </head>
-    <body>
-        <h1>Po kelių dienų kils barnis?</h1>
-        <p>
-            Po <?php
-            print $siukslines_turis / $siuksliu_turis_avg_per_day;
-            ?> 
-            dienų pirk gėlių ir šampano, jeigu nori, kad žmona šiukšles pati išneštų
-        </p>
+    <body style="text-align: center; background-color: coral;">
+        <img src="media/bomba.gif">
+            <?php
+        print date("s");
+        ?>
     </body>
-</html>
-
-
-
+    </html>
