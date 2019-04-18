8d1029d083e5357539e55865212d05a945ab3163
Kasparas Bareika <kasparas12@gmail.com>
2019-04-18 16:50:16 +0300

    IF ELSE

--- public_html/index.php
+++ public_html/index.php
@@ -1,19 +1,27 @@
 <?php
 
-$page = $_SERVER['PHP_SELF'];
-$sec = "1";
+    $grizai_velai = rand(0,1);
+    $grizai_isgeres = rand(0, 1);
 
+    if ($grizai_isgeres === $grizai_velai){
+        $text = 'miegosi ant sofos';
+    } elseif (!$grizai_velai) {
+        $text = 'Nemiegam ant sofos, jeigu grįžau nė vėlai';
+    } elseif (!$grizai_isgeres){
+        $text = 'Nemiegosi ant sofos jei grįžau blaivas';
+    } else {
+        $text = 'Nepisk iš namų ne alkaše';
+    }
 ?>
 <!DOCTYPE html>
 <html>
     <head>
         <meta charset="UTF-8">
-    <meta http-equiv="refresh" content="<?php echo $sec?>;URL='<?php echo $page?>'">
     </head>
-    <body style="text-align: center; background-color: coral;">
-        <img src="media/bomba.gif">
-            <?php
-        print date("s");
-        ?>
+    <body>
+    <img src="https://media1.giphy.com/media/9JOMxLFjCWWSk/giphy.gif" style="height: 200px;" alt="mašina">
+       <h1>
+        <?php print $text; ?>
+       </h1>
     </body>
-    </html>
+</html>
