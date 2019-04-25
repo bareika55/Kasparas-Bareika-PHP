06d801dc22cd368f2bfbe583d3b5eaad01c4b963
Kasparas <kasparas12@gmail.com>
2019-04-25 15:15:22 +0300

    masyvas

--- public_html/index.php
+++ public_html/index.php
@@ -1,24 +1,27 @@
 <?php
 
-$vaisiai = ['obuoliai', 'apelsinai', 'cukinija'
-
+$dideli_produktai = [
+        'kiausiniai' => 'Du dideli kiaušiniai',
+        'grietine' => 'didelis grietinės indelis'
 ];
-var_dump($vaisiai);
-?>
+$mazi_produktai = ['baklažanas'];
 
+var_dump($dideli_produktai,$mazi_produktai);
+
+?>
 
 <!doctype html>
 <html>
-<head>
-    <meta charset="UTF-8"
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="includes/grid.css">
-    <link rel="stylesheet" href="includes/normalize.css">
-    <title>Puslapio pavadinimas</title>
-</head>
-<body>
-</body>
+    <head>
+        <meta charset="UTF-8"
+        <meta name="viewport"
+              content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link rel="stylesheet" href="includes/grid.css">
+        <link rel="stylesheet" href="includes/normalize.css">
+        <title>Puslapio pavadinimas</title>
+    </head>
+    <body>
+    </body>
 </html>
 
