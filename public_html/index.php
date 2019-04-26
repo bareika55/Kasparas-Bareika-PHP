5ff2fe777e4571bcbacb8a6799b5060bbf75fbc9
Kasparas <kasparas12@gmail.com>
2019-04-26 14:37:07 +0300

    for each memory draugai ir aš

--- public_html/index.php
+++ public_html/index.php
@@ -1,26 +1,60 @@
 <?php
 
-$array = [20, 48, 45, 37, 48];
-foreach ($array as $value){
+$draugo_atmintis = [
+        'penktadienis',
+        'paskaitos',
+        'alus',
+        'dar vienas alutis',
+        'tualetas',
+        'cigarete',
+        'alutis',
+        'sokiai',
+];
+$mano_atmintis = [
+    'Penktadienis',
+    'Paskaita',
+    'Baras',
+    'Viskis',
+    'Alus',
+    'Degtine',
+    'Alus',
+    'Pirmadienis',
+    'Paskaita'
+];
 
-}
-print array_sum($array);
+$rand_flashback = rand(0, count($mano_atmintis) - 1);
+
 
 ?>
-
-<!doctype html>
+<!DOCTYPE html>
 <html>
     <head>
-        <meta charset="UTF-8"
-        <meta name="viewport"
-              content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <link rel="stylesheet" href="includes/grid.css">
-        <link rel="stylesheet" href="includes/normalize.css">
-        <title>Puslapio pavadinimas</title>
+        <title>Klases daras</title>
+        <meta charset="utf-8">
+        <link rel="stylesheet" type="text/css" href="style.css">
     </head>
     <body>
-    <img src="http://3.bp.blogspot.com/-6ZsyitckH4g/VR2h3dRDzXI/AAAAAAAAAkM/GYMPOYGjc04/s1600/15549_638741939589553_2113540796558574343_n.jpg" alt="as">
+        <h1>Kas buvo penktadienį ?!</h1>
+        <h2>Mano atmintis</h2>
+        <p>
+        <ul> <!-- foreach -->
+            <?php foreach($mano_atmintis as $value): ?>
+                <li>
+                    <?php print $value; ?>
+                </li>
+            <?php endforeach; ?>
+        </ul><!-- endforeach -->
+        </p>
+        <?php
+        print '#' .$rand_flashback . ': ' . $mano_atmintis[$rand_flashback] ;
+        ?>
+        <h2>Draugo atmintis</h2>
+        <ul> <!-- foreach -->
+            <?php foreach($draugo_atmintis as $value): ?>
+                <li>
+                    <?php print $value; ?>
+                </li>
+            <?php endforeach; ?>
+        </ul><!-- endforeach -->
     </body>
 </html>
-
