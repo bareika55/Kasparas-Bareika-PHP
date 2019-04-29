e0f8d34ee6d3b0d95b59be575cea8184cdcdf0a7
Kasparas <kasparas12@gmail.com>
2019-04-29 14:12:00 +0300

    FIG ATASKAITA

--- public_html/index.php
+++ public_html/index.php
@@ -1,60 +1,35 @@
 <?php
 
-$draugo_atmintis = [
-        'penktadienis',
-        'paskaitos',
-        'alus',
-        'dar vienas alutis',
-        'tualetas',
-        'cigarete',
-        'alutis',
-        'sokiai',
+$ataskaita = [
+        [
+         'name' => 'Rimi darbo užmokestis',
+        'amount' => 800,
+        ],
+    [
+      'name' => 'Traukinio bilietas',
+      'amount' => -10,
+    ],
+    [
+        'name' => 'Čili pica',
+        'amount' => -25,
+    ],
+    [
+        'name' => 'Dviračio remontas',
+        'amount'=> -115,
+    ],
 ];
-$mano_atmintis = [
-    'Penktadienis',
-    'Paskaita',
-    'Baras',
-    'Viskis',
-    'Alus',
-    'Degtine',
-    'Alus',
-    'Pirmadienis',
-    'Paskaita'
-];
-
-$rand_flashback = rand(0, count($mano_atmintis) - 1);
-
 
 ?>
-<!DOCTYPE html>
+<!doctype html>
 <html>
-    <head>
-        <title>Klases daras</title>
-        <meta charset="utf-8">
-        <link rel="stylesheet" type="text/css" href="style.css">
-    </head>
-    <body>
-        <h1>Kas buvo penktadienį ?!</h1>
-        <h2>Mano atmintis</h2>
-        <p>
-        <ul> <!-- foreach -->
-            <?php foreach($mano_atmintis as $value): ?>
-                <li>
-                    <?php print $value; ?>
-                </li>
-            <?php endforeach; ?>
-        </ul><!-- endforeach -->
-        </p>
-        <?php
-        print '#' .$rand_flashback . ': ' . $mano_atmintis[$rand_flashback] ;
-        ?>
-        <h2>Draugo atmintis</h2>
-        <ul> <!-- foreach -->
-            <?php foreach($draugo_atmintis as $value): ?>
-                <li>
-                    <?php print $value; ?>
-                </li>
-            <?php endforeach; ?>
-        </ul><!-- endforeach -->
-    </body>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Pirmadienis (array)</title>
+</head>
+<body>
+
+</body>
 </html>
