0222b70c6b5413d153cd52f603346a9b3314aeab
Kasparas <kasparas12@gmail.com>
2019-05-02 11:59:00 +0300

    fukncija ŽODŽIŲ ILGIS, MB_STRCUT, MB_STRLEN

--- public_html/index.php
+++ public_html/index.php
@@ -1,14 +1,25 @@
 <?php
 
-$avarage = [24, 24, 47, 88, 98];
-
-function avg($array){
-    $suma = 0;
-    foreach ($array as $value){
-        $suma += $value;
+function tekstas ($text){
+    if(mb_strlen($text)<= 100){
+        return $text;
     }
-    return $suma / count($array);
+    return mb_strcut($text, 0, 97) . '...';
 }
-print avg($avarage);
-?>
+$text2 = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda, aut commodi eaque eligendi error et fugiat labore maxime modi nulla odio, officia pariatur quae quidem repellat sapiente, sint. Omnis, praesentium.';
 
+?>
+<!doctype html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport"
+              content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link rel="stylesheet" href="style.css">
+        <title>Document</title>
+    </head>
+    <body>
+    <?php print tekstas($text2); ?>
+    </body>
+</html>
