dde7fbf256afe6b265616dbb20fa1fc9937766a2
Kasparas <kasparas12@gmail.com>
2019-05-06 10:52:39 +0300

    Function ($Array) ($Color) su foreach !

--- public_html/index.php
+++ public_html/index.php
@@ -1,25 +1,46 @@
 <?php
 
-function tekstas ($text){
-    if(mb_strlen($text)<= 100){
-        return $text;
-    }
-    return mb_strcut($text, 0, 97) . '...';
-}
-$text2 = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda, aut commodi eaque eligendi error et fugiat labore maxime modi nulla odio, officia pariatur quae quidem repellat sapiente, sint. Omnis, praesentium.';
+function arrayView ($array_key, $color_key){
 
+    $array = [
+        'labas',
+        'krabas',
+        'kalmaras',
+        'storas'
+    ];
+    $color = [
+        'black',
+        'red',
+        'white',
+        'blue',
+        'green',
+        'yellow'
+    ];
+
+    foreach ($array as $key => $value){
+        $tekstas = $array[$array_key];
+    }
+
+    foreach ($color as $key => $value){
+        $spalva = $color[$color_key];
+    }
+    return $tekstas . ' ' . $spalva;
+}
+print arrayView(3, 3);
 ?>
-<!doctype html>
 <html>
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport"
-              content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <link rel="stylesheet" href="style.css">
-        <title>Document</title>
-    </head>
-    <body>
-    <?php print tekstas($text2); ?>
-    </body>
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="includes/style.css">
+    <link rel="stylesheet" href="includes/normalize.css">
+    <link rel="stylesheet" href="includes/grid.css">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Hey Hey</title>
+
+</head>
+<body>
+
+</body>
 </html>
