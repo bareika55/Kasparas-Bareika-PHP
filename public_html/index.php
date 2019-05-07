ef2daef4ea8226378124b169aaa3c113e04301b5
Kasparas <kasparas12@gmail.com>
2019-05-07 17:27:34 +0300

    SHEEP foreach, UNSET($SHEEP)

--- public_html/index.php
+++ public_html/index.php
@@ -4,6 +4,14 @@ $sheep = ['bleeedee KURWA'];
 
 for ($x = 0; $x < 5; $x++) {
     $sheep[] = &$sheep[$x];
+
 }
+
+foreach ($sheep as $key => $value) {
+    unset($sheep[$key]);
+    $sheep[$key] = $value;
+}
+
+$sheep[3] = 'Neprisikiškekopūsteliaudavome';
 
 var_dump($sheep);
