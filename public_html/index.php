e208843a264a2d19867b99aaf5c616dab99f2ea1
Kasparas <kasparas12@gmail.com>
2019-05-08 16:08:06 +0300

    INPUT FORMS ACTION

--- public_html/index.php
+++ public_html/index.php
@@ -1,17 +1,26 @@
 <?php
 
-$sheep = ['bleeedee KURWA'];
+var_dump($_POST);
 
-for ($x = 0; $x < 5; $x++) {
-    $sheep[] = &$sheep[$x];
-
+if (!empty($_POST)) {
+    $skaicius = $_POST['action'];
+    $skaicius += 1 + $skaicius;
+} else {
+    $skaicius = 0;
 }
 
-foreach ($sheep as $key => $value) {
-    unset($sheep[$key]);
-    $sheep[$key] = $value;
-}
-
-$sheep[3] = 'Neprisikiškekopūsteliaudavome';
-
-var_dump($sheep);
+?>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport"
+              content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Document</title>
+    </head>
+    <body>
+        <form method="post">
+            <button name="action" value="<?php print $skaicius; ?>"><?php print $skaicius; ?></button>
+        </form>
+    </body>
+</html>
