1c5afeff15c61202433bd2a3dcde03689b340de4
Kasparas <kasparas12@gmail.com>
2019-05-09 15:22:45 +0300

    SECURITY FORM (INPUT) with IF and ELSE

--- public_html/index.php
+++ public_html/index.php
@@ -1,13 +1,18 @@
 <?php
 
-$hack = $_POST['hack_me'] ?? 'Try!';
 $hack = filter_input_array(INPUT_POST, [
     'hack_me' => FILTER_SANITIZE_SPECIAL_CHARS]);
+if ($hack)
+{  $text = $hack['hack_me'];
+} else {
+    $text = 'UŽEIKITE Į SVEČIUS';
 
+}
+var_dump($hack);
 ?>
 <html>
 <body>
-<h1><?php print $hack; ?></h1>
+<h1><?php print $text; ?></h1>
 <form method="post">
     <input type="text" name="hack_me" placeholder="Hack Me" required>
     <button name="button" value="push">Try</button>
