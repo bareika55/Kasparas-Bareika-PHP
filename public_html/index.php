abc8b5198416af6587e7c8c04d913fe95d59f8ec
Kasparas <kasparas12@gmail.com>
2019-05-09 14:45:45 +0300

    HACKED

--- public_html/index.php
+++ public_html/index.php
@@ -1,44 +1,16 @@
 <?php
 
-$sertifikatas = $_POST['sertifikatas'];
-$atsakymas = '';
-
-if (isset($_POST['action'])) {
-    $atsakymas = $_POST['action'];
-} else {
-    print 'Užpildyk laukelius';
-}
+$hack = $_POST['hack_me'] ?? 'Try!';
+$hack = filter_input_array(INPUT_POST, [
+    'hack_me' => FILTER_SANITIZE_SPECIAL_CHARS]);
 
 ?>
-<!DOCTYPE html>
 <html>
-<head>
-    <title>Kazkas bl</title>
-    <meta charset="utf-8">
-    <link rel="stylesheet" href="includes/style.css">
-</head>
 <body>
-<form action="index.php" method="post">
-    <label>
-        <input type="text" name="vardas" placeholder="Vardas" required>
-        <input type="text" name="pavarde" placeholder="Pavardė" required>
-        <input type="number" name="amzius" min="1" max="99" placeholder="Amžius">
-        <select name="sertifikatas">
-            <option value="Pradedantysis" selected>Pradedantysis</option>
-            <option value="Pažengęs">Pažengęs</option>
-            <option value="Profesionalas">Profesionalas</option>
-        </select>
-        <button name="action" value="submit">Send</button>
-    </label>
-    <ul>
-        <li>
-            <h1>Tavo vardas:</h1><span><?php print $_POST['vardas']; ?></span>
-            <h1>Tavo pavardė:</h1><span><?php print $_POST['pavarde']; ?></span>
-            <h1>Tavo amžius:</h1> <span><?php print $_POST['amzius']; ?></span>
-            <h1>Tavo IT lygis:</h1> <span><?php print $sertifikatas; ?></span>
-        </li>
-    </ul>
-
+<h1><?php print $hack; ?></h1>
+<form method="post">
+    <input type="text" name="hack_me" placeholder="Hack Me" required>
+    <button name="button" value="push">Try</button>
+</form>
 </body>
 </html>
-
