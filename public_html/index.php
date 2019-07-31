5673ab872c9b362f13db4740bae106ff1156f349
Kasparas <kasparas12@gmail.com>
2019-07-31 11:49:35 +0300

    Abstract/ Extented Classes !

--- public_html/index.php
+++ public_html/index.php
@@ -1,81 +1,169 @@
 <?php
 
-$form_array = [
-    'action' => 'index.php',
-    'method' => 'POST',
+require '../bootloader.php';
+
+$whiskey = new \App\Drinks\StrongDrink();
+
+$whiskey->setAmount(700);
+$whiskey->drink();
+
+$pivasikas = new \App\Drinks\LightDrink();
+$pivasikas->setAmount(500);
+$pivasikas->drink();
+var_dump($whiskey->getAmount(), $pivasikas->getAmount());
+
+
+
+$form = [
+    'attr' => [
+        'method' => 'POST'
+    ],
     'fields' => [
-        'first_name' => [
-            'label' => 'Vardas',
+        'name' => [
+            'label' => 'Name',
             'type' => 'text',
-            'value' => '',
-            'error' => 'Klaida!',
-            'validator' => [
-                'validate_not_empty',
-            ],
-            'filter' => FILTER_SANITIZE_NUMBER_INT,
-            'placeholder' => 'Onutė'
+            'extra' => [
+                'validators' => [
+                    'validate_not_empty',
+                ]
+            ]
         ],
-        'second_name' => [
-            'label' => 'Pavardė',
+        'amount_ml' => [
+            'label' => 'Amount ML',
+            'type' => 'number',
+            'extra' => [
+                'validators' => [
+                    'validate_not_empty'
+                ]
+            ]
+        ],
+        'abarot' => [
+            'label' => 'Abarot',
+            'type' => 'number',
+            'extra' => [
+                'validators' => [
+                    'validate_not_empty'
+                ]
+            ]
+        ],
+        'image' => [
+            'label' => 'Image',
             'type' => 'text',
-            'value' => '',
-            'placeholder' => 'Kimarinskienė'
+            'extra' => [
+                'validators' => [
+                    'validate_not_empty'
+                ]
+            ]
         ],
+    ],
+    'buttons' => [
+        'submit' => [
+            'title' => 'Submituoti',
+            'extra' => [
+                'attr' => [
+                    'class' => 'blue-btn'
+                ]
+            ]
+        ]
+    ],
+    'callbacks' => [
+        'success' => 'form_success',
+        'fail' => 'form_fail'
+    ],
+];
+
+$filtered_input = get_form_input($form);
+if (!empty($filtered_input)) {
+    validate_form($filtered_input, $form, $modelDrinks);
+}
+
+function form_success($filtered_input, &$form, $modelDrinks)
+{
+    $modelDrinks->insert(new App\Drinks\Drink($filtered_input));
+}
+
+function form_fail()
+{
+    print 'fail';
+}
+
+$nav = [
+    'left' => [
+        ['url' => 'index.php', 'title' => 'Home'],
+        ['url' => 'register.php', 'title' => "Registration"]
     ]
 ];
 
+//$drink = new App\Drinks\Drink();
+//$drink->setData([
+//    'name' => 'gira',
+//    'amount_ml' => 1000,
+//    'abarot' => 1.2,
+//    'image' => 'https://www.barbora.lt/api/Images/GetInventoryImage?id=42e6bcb2-bf0e-4f1c-88ff-08a9dc411321',
+//]);
 
-function get_form_input($form_array)
-{
-    $filter_parameters = [];
-    foreach ($form_array['fields'] as $key => $field_values) {
-        $filter_parameters[$key] = FILTER_SANITIZE_SPECIAL_CHARS;
-        if (isset($field_values['filter'])) {
-            $filter_parameters = $field_values['filter'];
-        } else {
-            $filter_parameters[$key] = FILTER_SANITIZE_SPECIAL_CHARS;
-        }
-    }
-    return filter_input_array(INPUT_POST, $filter_parameters);
+$gira = new App\Drinks\Drink();
+$gira->setData([
+    'name' => 'Gira',
+    'amount_ml' => 1000,
+    'abarot' => 1.2,
+    'image' => 'no_link'
+]);
+
+$sidras = new App\Drinks\Drink();
+$sidras->setData([
+    'name' => 'Sidriukas',
+    'amount_ml' => 330,
+    'abarot' => 5.5,
+    'image' => 'no_link'
+]);
+
+$vanduo = new App\Drinks\Drink();
+$vanduo->setData([
+    'name' => 'Neptunas',
+    'amount_ml' => 1000,
+    'abarot' => 0,
+    'image' => 'no_link'
+]);
+
+$gaivusis_gerimas = new App\Drinks\Drink();
+$gaivusis_gerimas->setData([
+    'name' => 'Coca-Cola',
+    'amount_ml' => 1000,
+    'abarot' => 0,
+    'image' => 'no_link'
+]);
+
+
+$modelDrinks = new \App\Drinks\Model();
+//$modelDrinks->insert($gira);
+
+
+$drinks = $modelDrinks->get(['abarot' => 40]);
+
+foreach ($drinks as $drink) {
+    var_dump($drink);
+    $gira->setImage('kazkoks pvz');
+    $modelDrinks->update($drink);
 }
 
-;
-
-$safe_input = get_form_input($form_array);
-validate_not_empty($safe_input['first_name'], $form_array['fields']['first_name']);
-validate_not_empty($safe_input['second_name'], $form_array['fields']['second_name']);
-
-function validate_not_empty($field_input, &$field)
-{
-    if (strlen($field_input) == 0) {
-        $field['error'] = 'neteisingai įvesta informacija';
-    } else {
-        return true;
-    }
-}
-
-
-var_dump(get_form_input($form_array));
 ?>
 <html>
 <head>
     <meta charset="UTF-8">
-    <title>06.25</title>
-    <link rel="stylesheet" type="text/css" href="includes/normalize.css">
-    <link rel="stylesheet" type="text/css" href="includes/style.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>OOP</title>
+    <link rel="stylesheet" href="media/css/normalize.css">
+    <link rel="stylesheet" href="media/css/style.css">
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+    <link rel="icon" href="favicon.ico" type="image/x-icon">
+    <script defer src="media/js/app.js"></script>
 </head>
 <body>
-<form action="<?php print $form_array['action']; ?>" method="<?php print $form_array['method']; ?>">
-    <?php foreach ($form_array['fields'] as $key => $input): ?>
-        <label><?php print $input['label']; ?></label>
-        <input name="<?php print $key; ?>" type="<?php print $input['type']; ?>"
-               placeholder="<?php print $input['placeholder']; ?>">
-        <?php if (isset($input['error'])): ?>
-            <span><?php print $input['error']; ?></span>
-        <?php endif; ?>
-    <?php endforeach; ?>
+<?php require ROOT . '/app/templates/navigation.tpl.php'; ?>
 
-    <button>Submit</button>
-</form>
+<div class="content">
+    <?php require ROOT . '/core/templates/form/form.tpl.php'; ?>
+</div>
 </body>
 </html>
